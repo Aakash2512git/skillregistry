0df186d7b05f362a0af389364a487c76386538be
AkashShukla <AkashShukla@ibm.com>
2026-07-09 19:30:35 +0530

    Bump version to 0.1.1 for README benchmark release.

--- src/skillregistry/__init__.py
+++ src/skillregistry/__init__.py
@@ -4,4 +4,4 @@ from skillregistry.models import SkillDocument, SkillMatch, SkillRecord
 from skillregistry.registry import SkillRegistry
 
 __all__ = ["SkillDocument", "SkillMatch", "SkillRecord", "SkillRegistry"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
